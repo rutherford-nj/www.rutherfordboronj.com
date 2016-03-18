a3424a029317361ce3a9b1b909b83b175b507f86
James Bogosian <bogosj@gmail.com>
2016-03-18 19:44:40 -0400

    double check whether reload is necessary

--- static/js/reload.js
+++ static/js/reload.js
@@ -1,6 +1,7 @@
 (function($){
+window['09c8e5e290dea387fb2496197d117667b73012ff'] = true;
 
-if (!$('#reload_not_needed').length) {
+if (!$('#09c8e5e290dea387fb2496197d117667b73012ff').length) {
   document.location.reload(true);
 }
 
