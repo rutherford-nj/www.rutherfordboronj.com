6dbeb51f637d431b460a946c348e869254ec0046
James Bogosian <bogosj@users.noreply.github.com>
2016-06-18 15:05:48 -0400

    Create github-www.user.js

---
+++ scripts/github-www.user.js
@@ -0,0 +1,11 @@
+==UserScript==
+// @name         GitHub www checker script
+// @version      0.0.1
+// @description  GitHub script to check if you are editing the www branch.
+// @match        *://github.com/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js
+// @updateURL    https://raw.githubusercontent.com/StylishThemes/GitHub-Dark-Script/master/github-dark-script.user.js
+// @downloadURL  https://raw.githubusercontent.com/StylishThemes/GitHub-Dark-Script/master/github-dark-script.user.js
+// ==/UserScript==
+
+alert(window.location);
