0e24fcf7203f9f34a15f60c56916d5d9b82ae26d
James Bogosian <bogosj@users.noreply.github.com>
2016-06-18 15:44:14 -0400

    Update github-www.user.js

--- scripts/github-www.user.js
+++ scripts/github-www.user.js
@@ -1,11 +1,17 @@
-==UserScript==
+// ==UserScript==
 // @name         GitHub www checker script
-// @version      0.0.1
+// @version      1.0.0
 // @description  GitHub script to check if you are editing the www branch.
 // @match        *://github.com/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js
-// @updateURL    https://raw.githubusercontent.com/StylishThemes/GitHub-Dark-Script/master/github-dark-script.user.js
-// @downloadURL  https://raw.githubusercontent.com/StylishThemes/GitHub-Dark-Script/master/github-dark-script.user.js
 // ==/UserScript==
 
-alert(window.location);
+(function($) {
+    $(document).ready(function() {
+        var loc = window.location.href;
+        if (loc.search('/(blob|tree)\/www/') > -1) {
+            var content = $('body');
+            content.prepend('<div style="color:red;font-size:5em;text-align:center;">You are editing the WWW branch</div>');
+        }
+    });
+})(jQuery);
