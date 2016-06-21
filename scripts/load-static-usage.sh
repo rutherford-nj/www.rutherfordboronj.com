d183a0989b72353f6c8604f73c1053af3aa1dc1e
James Bogosian <bogosj@gmail.com>
2016-06-21 13:45:33 -0400

    Add script for static bucket use analysis.

---
+++ scripts/load-static-usage.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+bq rm storageanalysis.static_usage
+bq mk storageanalysis
+curl -o /dev/shm/cloud-schema http://storage.googleapis.com/pub/cloud_storage_usage_schema_v0.json
+bq load --skip_leading_rows=1 storageanalysis.static_usage \
+  gs://static-dot-rutherford-nj-dot-com-storagelogs/static.rutherford-nj.com_usage* \
+  /dev/shm/cloud-schema
