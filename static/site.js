9280e4d9f852d72a1ab0a26bf489261bdac0a3f5
James Bogosian <bogosj@users.noreply.github.com>
2016-12-12 10:11:02 -0500

    Regenerate Google API key

--- static/site.js
+++ static/site.js
@@ -3,7 +3,7 @@
 
 jQuery(function() {
 
-window._apiKey = 'AIzaSyAmCRiynk_4e6YksIv7P_XSt4Mmi4gwp-c';
+window._apiKey = 'AIzaSyAI2xpusgBrNbB-AmPUcotmJ5Sd0bllqfQ';
 
 {% include_relative js/jscache/cache.js %}
 
