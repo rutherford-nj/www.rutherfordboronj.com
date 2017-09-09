dd2e86d818264599c3ffb2f01f05dc853d82269f
James Bogosian <bogosj@users.noreply.github.com>
2017-09-09 16:12:19 -0400

    Update site.js

--- static/site.js
+++ static/site.js
@@ -12,7 +12,5 @@ window._apiKey = 'AIzaSyAf95jYqAyeglXVXy9LMmDX8Py0wnGipKU';
 {% include_relative js/cloud-storage.js %}
 
 {% include_relative js/spreadsheet-content.js %}
-
-{% include_relative js/reload.js %}
   
 });
