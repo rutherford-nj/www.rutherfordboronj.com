8351cd76fd64658d7159c7fc13db88b01ef78a0b
James Bogosian <bogosj@gmail.com>
2018-07-01 13:47:11 -0400

    Update homepage images to rotate.

--- static/js/homepage_rotate.js
+++ static/js/homepage_rotate.js
@@ -1,25 +1,5 @@
 (function($){
 
-if (!$('.homepage-images').length) {
-  return;
-}
-
-var srcs = [];
-var alts = [];
-
-{% for image in site.data.homepage_images %}
-  srcs.push('{{ image.src }}');
-  alts.push('{{ image.alt }}');
-{% endfor %}
-
-var indexKey = 'currentHomepageImageIndex';
-var currentIndex = window.localStorage.getItem(indexKey) || 0;
-currentIndex = parseInt(currentIndex, 10);
-var nextIndex = currentIndex + 1;
-nextIndex = nextIndex % srcs.length;
-window.localStorage.setItem(indexKey, nextIndex);
-
-$('.homepage-images').empty();
-$('.homepage-images').append('<div><img src="'+srcs[currentIndex]+'" alt="'+alts[currentIndex]+'"></div>');
+$('#homepage-rotation').slick();
 
 })(jQuery);
