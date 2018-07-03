a2d97654d9dcdeb1467e067e3405486fd0331859
James Bogosian <bogosj@gmail.com>
2018-07-03 13:39:09 -0400

    Only add H2 elements to the ToC.

--- static/js/community_outreach_toc.js
+++ static/js/community_outreach_toc.js
@@ -4,30 +4,19 @@ if (!$('#community-outreach-toc').length) {
   return;
 }
 
-var lastElementType;
 var tocHtml = '<ol>';
 
-$('.interior-page-content > h2, h3').each(function() {
+$('.interior-page-content > h2').each(function() {
 	var elt = $(this);
 	if (!elt.attr('id')) {
 		return;
 	}
-	if (elt.is('h3')) {
-		if (lastElementType === 'h2') {
-			tocHtml += '<ol>';
-		}
-		lastElementType = 'h3';
-	}
 	if (elt.is('h2')) {
-		if (lastElementType === 'h3') {
-			tocHtml += '</ol>';
-		}
-		lastElementType = 'h2';
+		tocHtml += '<li><a href="#' + elt.attr('id') + '">' + elt.text() + '</a></li>';
 	}
-	tocHtml += '<li><a href="#' + elt.attr('id') + '">' + elt.text() + '</a></li>';
 });
 
-tocHtml += "</ol></ol>";
+tocHtml += "</ol>";
 console.log(tocHtml);
 
 $(tocHtml).appendTo($('#community-outreach-toc'));
