a5975336e4ca14efdf3a7e561dec12e3da10bd35
James Bogosian <bogosj@users.noreply.github.com>
2018-08-04 11:03:52 -0400

    Update community-outreach-toc.js

--- static/js/community-outreach-toc.js
+++ static/js/community-outreach-toc.js
@@ -8,9 +8,11 @@ var tocHtml = '<ol>';
 
 $('.interior-page-content > h2').each(function() {
 	var elt = $(this);
+  
 	if (!elt.attr('id')) {
 		return;
 	}
+  
 	if (elt.is('h2')) {
 		tocHtml += '<li><a href="#' + elt.attr('id') + '">' + elt.text() + '</a></li>';
 	}
