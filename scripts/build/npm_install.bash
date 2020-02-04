d2cf93893673aa2c606a8b4fdeaa38560a88837f
James Bogosian <bogosj@gmail.com>
2020-02-04 13:39:16 -0500

    SW

--- scripts/build/npm_install.bash
+++ scripts/build/npm_install.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Set workspace for local builds.
+[[ "$SITE_WORKSPACE" == "" ]] && \
+    SITE_WORKSPACE=`pwd`
+
 if [ -d node_modules ]; then
   echo "node_modules exists"
 else
   docker run \
-	-v $(SITE_WORKSPACE):/srv/jekyll \
+	-v $SITE_WORKSPACE:/srv/jekyll \
 	jekyll/builder:latest /bin/bash -c "npm install"
 fi
