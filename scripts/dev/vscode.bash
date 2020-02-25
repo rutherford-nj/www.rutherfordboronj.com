da0e2105ee41f2b18b7217ca12ef8a56166e40c8
James Bogosian <bogosj@gmail.com>
2020-02-25 12:29:13 -0500

    Add server password input for vscode.

--- scripts/dev/vscode.bash
+++ scripts/dev/vscode.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -it --rm -p 127.0.0.1:8080:8080 -v "$PWD:/home/coder/rutherford" codercom/code-server
+read -s -p "Server password: " SERVER_PASSWORD
+docker run -it --env PASSWORD=$SERVER_PASSWORD --rm -p 127.0.0.1:8080:8080 -v "$PWD:/home/coder/rutherford" codercom/code-server
