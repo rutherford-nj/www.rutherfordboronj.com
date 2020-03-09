ede884bb941f8dda0cbf91ae484a9950d039f94b
James Bogosian <bogosj@users.noreply.github.com>
2020-03-08 20:18:41 -0400

    Twitter for IE (#113)
    * Fix Twitter embeds in IE.

---
+++ static/js/twitter.js
@@ -0,0 +1,7 @@
+(function ($) {
+    var show = function () {
+        $("a.twitter-timeline").css("display", "block");
+    };
+    
+    window.setTimeout(show, 3000);
+})(jQuery);
--- static/site.js
+++ static/site.js
@@ -12,6 +12,8 @@ window._apiKey = 'AIzaSyCskHR72T77cygXue7RA8YVbTj6x7wLx2o';
 {% include_relative js/spreadsheet-content.js %}
 
 {% include_relative js/community-outreach-toc.js %}
-  
+
+{% include_relative js/twitter.js %}
+
 });
 
