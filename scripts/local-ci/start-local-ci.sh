7b5df2829999b6443055e67c4466237dc2768594
James Bogosian <bogosj@gmail.com>
2020-08-31 15:56:19 -0400

    cleanup

--- scripts/local-ci/start-local-ci.sh
+++ scripts/local-ci/start-local-ci.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-docker build --tag=rutherford-site-webhook scripts/local-ci
+set -x
 
 echo "Provide webhook secret:"
 read WEBHOOK_SECRET
+
+docker build --tag=rutherford-site-webhook scripts/local-ci
 
 docker run -d -p 58778:9000 --name=rutherford-site-webhook \
 	-v `pwd`:/site \
