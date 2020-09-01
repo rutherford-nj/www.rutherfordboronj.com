62632246d61132425f53984d41b5cf386716ea5c
James Bogosian <bogosj@users.noreply.github.com>
2020-09-01 10:11:24 -0400

    Update start-local-ci.sh

--- scripts/local-ci/start-local-ci.sh
+++ scripts/local-ci/start-local-ci.sh
@@ -6,9 +6,8 @@ echo "Provide webhook secret:"
 read WEBHOOK_SECRET
 
 docker container stop rutherford-site-webhook rutherford-site-server
-docker image prune -a
 
-docker build --tag=rutherford-site-webhook scripts/local-ci
+docker build --no-cache --tag=rutherford-site-webhook scripts/local-ci
 
 docker run --rm -d -p 58778:9000 --name=rutherford-site-webhook \
 	-v `pwd`:/site \
