870f717400503e372bb975d3725b28ed4dd15f6e
James Bogosian <bogosj@gmail.com>
2020-10-23 21:26:41 -0400

    Trim response so there isn't a space before the period.

--- static/js/covid-charts-last-updated.js
+++ static/js/covid-charts-last-updated.js
@@ -5,6 +5,7 @@
         elt = $(elt);
         $.get("https://grecine.github.io/covid_data//last_updated")
             .done(function (data) {
+                data = $.trim(data);
                 elt.text(`The charts were last updated on ${data}.`);
             });
     });
