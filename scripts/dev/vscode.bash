a49f6d595990cef27a965229c86c48dcab760fe2
James Bogosian <bogosj@users.noreply.github.com>
2020-11-19 11:26:51 -0500

    Delete vscode.bash

--- scripts/dev/vscode.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -s -p "Server password: " SERVER_PASSWORD
-docker run -it --env PASSWORD=$SERVER_PASSWORD --rm -p 0.0.0.0:8080:8080 \
-  -v ~/.bash_aliases:/home/coder/.bash_aliases \
-  -v ~/.gitconfig:/home/coder/.gitconfig \
-  -v ~/.ssh:/home/coder/.ssh \
-  -v "$PWD:/home/coder/rutherford" \
-  codercom/code-server
