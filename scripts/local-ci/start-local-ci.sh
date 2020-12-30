c75efe2269198e6e4d4844d736ffc06c0e28bc7e
James Bogosian <bogosj@gmail.com>
2020-12-30 12:46:45 -0500

    Build webhook docker image locally for CI.

--- scripts/local-ci/start-local-ci.sh
+++ scripts/local-ci/start-local-ci.sh
@@ -2,8 +2,12 @@
 
 set -x
 
+cd scripts/local-ci
+
+docker build . -t rutherford-site-webhook
+
 docker run --rm -d -p 58778:9000 \
     --name=rutherford-site-webhook \
     -v `pwd`:/site \
     --env-file=/keybase/team/rutherford_nj.website/local_ci.env \
-    ghcr.io/rutherford-nj/rutherford-site-webhook:latest
+    rutherford-site-webhook
