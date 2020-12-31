e86687d6a597641e0be21ec26d48aee93d42dacd
James Bogosian <bogosj@gmail.com>
2020-12-30 19:13:45 -0500

    Move all local-ci into script.

--- scripts/local-ci/start-local-ci.sh
+++ scripts/local-ci/start-local-ci.sh
@@ -2,12 +2,23 @@
 
 set -x
 
+docker container stop rutherford-site-webhook rutherford-site-server
+
+docker image rm rutherford-site-webhook
+
 cd scripts/local-ci
 
 docker build . -t rutherford-site-webhook
+
+cd ../../
 
 docker run --rm -d -p 58778:9000 \
     --name=rutherford-site-webhook \
     -v `pwd`:/site \
     --env-file=/keybase/team/rutherford_nj.website/local_ci.env \
     rutherford-site-webhook
+
+docker run -d --rm -p 0.0.0.0:58777:8777 \
+	--name=rutherford-site-server \
+	-v `pwd`:/srv/jekyll \
+	jekyll/builder:latest jekyll serve --watch -P 8777 -p /dev/null
