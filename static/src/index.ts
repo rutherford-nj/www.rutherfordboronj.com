271ef6656e8675a3a84c9cee304624248ed2cdb2
James Bogosian <bogosj@users.noreply.github.com>
2022-02-01 15:00:33 +0000

    Remove unused tab handlers.

--- static/src/index.ts
+++ static/src/index.ts
@@ -1,5 +1,4 @@
 import { applySearchHandlers } from "./search-handlers";
-import { applyTabHandlers } from "./homepage-tabs";
 import { buildCommunityOutreachTOC } from "./community-outreach";
 import { fetchCovidCharts } from "./covid-charts";
 import { fetchDynamicMessage } from "./dynamic-message";
@@ -7,7 +6,6 @@ import { findCloudStorageElements } from "./cloud-storage";
 import { runSearch } from "./search";
 
 applySearchHandlers();
-applyTabHandlers();
 buildCommunityOutreachTOC();
 fetchCovidCharts();
 fetchDynamicMessage();
