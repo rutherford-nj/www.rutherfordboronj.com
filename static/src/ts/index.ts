3500d579e2050c5418022074c52cc5854cee6052
James Bogosian <bogosj@users.noreply.github.com>
2022-09-02 17:23:20 +0000

    Convert banner to a _data file.

--- static/src/ts/index.ts
+++ static/src/ts/index.ts
@@ -1,7 +1,6 @@
 import { applySearchHandlers } from "./search-handlers";
 import { buildCommunityOutreachTOC } from "./community-outreach";
 import { fetchCovidCharts } from "./covid-charts";
-import { fetchDynamicMessage } from "./dynamic-message";
 import { findCloudStorageElements } from "./cloud-storage";
 import { initHomepageCarousel } from "./homepage";
 import { runSearch } from "./search";
@@ -9,7 +8,6 @@ import { runSearch } from "./search";
 applySearchHandlers();
 buildCommunityOutreachTOC();
 fetchCovidCharts();
-fetchDynamicMessage();
 findCloudStorageElements();
 initHomepageCarousel();
 runSearch();
