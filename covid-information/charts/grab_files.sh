9742204bd4094c02ae266111f9b5e171b6b3b6a7
James Bogosian <bogosj@users.noreply.github.com>
2022-09-27 16:18:48 +0000

    Freeze COVID charts page.

---
+++ covid-information/charts/grab_files.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm *svg
+wget https://rutherford-nj.github.io/data-committee.covid-data/new_cases_per_100K_incidence.svg
+wget https://rutherford-nj.github.io/data-committee.covid-data/new_cases_Rutherford_fitted_14d.svg
+wget https://rutherford-nj.github.io/data-committee.covid-data/new_cases_per_100K_3d_trajectory.svg
--- static/src/ts/index.ts
+++ static/src/ts/index.ts
@@ -1,13 +1,11 @@
 import { applySearchHandlers } from "./search-handlers";
 import { buildCommunityOutreachTOC } from "./community-outreach";
-import { fetchCovidCharts } from "./covid-charts";
 import { findCloudStorageElements } from "./cloud-storage";
 import { initHomepageCarousel } from "./homepage";
 import { runSearch } from "./search";
 
 applySearchHandlers();
 buildCommunityOutreachTOC();
-fetchCovidCharts();
 findCloudStorageElements();
 initHomepageCarousel();
 runSearch();
